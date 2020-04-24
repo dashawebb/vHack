d6d8fe6f627f5e4218882de7896789ad5c20ded2
Dasha Webb <DDmVebb@sberbank.ru>
2020-04-24 18:35:30 +0300

    setting up project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardPage from "./components/DashboardPage";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DashboardPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './DashboardPage.module.less'
+
+const DashboardPage = () => {
+    return (
+        <div className={styles.container}>
+            <span className={styles.title}>Я дашборд
+            </span>
+        </div>
+    )
+}
+
+export default DashboardPage
