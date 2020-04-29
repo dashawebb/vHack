74dcc0dbe7bf9353b96042336f4782341a29d80f
Dasha Webb <DDmVebb@sberbank.ru>
2020-04-29 19:37:03 +0300

    pre-deploy commit

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import styles from './DashboardPage.module.less'
+
+const API = ''
+
+const Dashboard = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        // fetch()
+    }, [])
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
+    const fetch = async () => {
+        try {
+            setIsLoading(true)
+            let res = await axios.get(`${API}`);
+            setData(res.data)
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div>
+            <span className={styles.title}>Приветики</span>
+        </div>
+    )
+}
 
 const DashboardPage = () => {
     return (
         <div className={styles.container}>
-            <span className={styles.title}>Я дашборд
-            </span>
+            <Dashboard />
         </div>
     )
 }
